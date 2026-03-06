74d9596880fb748ee3ec7b95c9491f8fd48808cf
RafaellMoralesL <25001309@galileo.edu>
2026-03-05 23:25:50 -0600

    Correccion entidades

---
+++ SQL Base de Datos/BDIII_Workbench.sql
@@ -0,0 +1,218 @@
+CREATE DATABASE  IF NOT EXISTS `sistema_inventario` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `sistema_inventario`;
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: sistema_inventario
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `almacen`
+--
+
+DROP TABLE IF EXISTS `almacen`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `almacen` (
+  `id_almacen` int NOT NULL AUTO_INCREMENT,
+  `nombre_almacen` varchar(50) NOT NULL,
+  `ubicacion_almacen` varchar(80) NOT NULL,
+  PRIMARY KEY (`id_almacen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `almacen`
+--
+
+LOCK TABLES `almacen` WRITE;
+/*!40000 ALTER TABLE `almacen` DISABLE KEYS */;
+/*!40000 ALTER TABLE `almacen` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `detalle_venta`
+--
+
+DROP TABLE IF EXISTS `detalle_venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `detalle_venta` (
+  `id_detalle` int NOT NULL AUTO_INCREMENT,
+  `id_venta` int NOT NULL,
+  `id_producto` int NOT NULL,
+  PRIMARY KEY (`id_detalle`),
+  KEY `id_venta` (`id_venta`),
+  KEY `id_producto` (`id_producto`),
+  CONSTRAINT `detalle_venta_ibfk_1` FOREIGN KEY (`id_venta`) REFERENCES `venta` (`id_venta`),
+  CONSTRAINT `detalle_venta_ibfk_2` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `detalle_venta`
+--
+
+LOCK TABLES `detalle_venta` WRITE;
+/*!40000 ALTER TABLE `detalle_venta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `detalle_venta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empleado`
+--
+
+DROP TABLE IF EXISTS `empleado`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleado` (
+  `id_empleado` int NOT NULL AUTO_INCREMENT,
+  `nombre_empleado` varchar(60) NOT NULL,
+  `correo_empleado` varchar(60) NOT NULL,
+  `password_empleado` varchar(25) DEFAULT NULL,
+  `id_rol` int NOT NULL,
+  PRIMARY KEY (`id_empleado`),
+  UNIQUE KEY `correo_empleado` (`correo_empleado`),
+  KEY `id_rol` (`id_rol`),
+  CONSTRAINT `empleado_ibfk_1` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleado`
+--
+
+LOCK TABLES `empleado` WRITE;
+/*!40000 ALTER TABLE `empleado` DISABLE KEYS */;
+/*!40000 ALTER TABLE `empleado` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `inventario`
+--
+
+DROP TABLE IF EXISTS `inventario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `inventario` (
+  `id_inventario` int NOT NULL AUTO_INCREMENT,
+  `id_producto` int NOT NULL,
+  `id_almacen` int NOT NULL,
+  `stock` int NOT NULL,
+  `ubicacion_interna` varchar(80) DEFAULT NULL,
+  PRIMARY KEY (`id_inventario`),
+  KEY `id_producto` (`id_producto`),
+  KEY `id_almacen` (`id_almacen`),
+  CONSTRAINT `inventario_ibfk_1` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`),
+  CONSTRAINT `inventario_ibfk_2` FOREIGN KEY (`id_almacen`) REFERENCES `almacen` (`id_almacen`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `inventario`
+--
+
+LOCK TABLES `inventario` WRITE;
+/*!40000 ALTER TABLE `inventario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `inventario` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `producto`
+--
+
+DROP TABLE IF EXISTS `producto`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `producto` (
+  `id_producto` int NOT NULL AUTO_INCREMENT,
+  `codigo_serie` varchar(50) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `descripcion` varchar(80) DEFAULT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`id_producto`),
+  UNIQUE KEY `codigo_serie` (`codigo_serie`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producto`
+--
+
+LOCK TABLES `producto` WRITE;
+/*!40000 ALTER TABLE `producto` DISABLE KEYS */;
+/*!40000 ALTER TABLE `producto` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rol`
+--
+
+DROP TABLE IF EXISTS `rol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `rol` (
+  `id_rol` int NOT NULL AUTO_INCREMENT,
+  `nombre_rol` varchar(50) NOT NULL,
+  `descripcion_rol` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id_rol`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rol`
+--
+
+LOCK TABLES `rol` WRITE;
+/*!40000 ALTER TABLE `rol` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `venta`
+--
+
+DROP TABLE IF EXISTS `venta`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `venta` (
+  `id_venta` int NOT NULL AUTO_INCREMENT,
+  `fecha` datetime NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `id_empleado` int NOT NULL,
+  PRIMARY KEY (`id_venta`),
+  KEY `id_empleado` (`id_empleado`),
+  CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`id_empleado`) REFERENCES `empleado` (`id_empleado`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `venta`
+--
+
+LOCK TABLES `venta` WRITE;
+/*!40000 ALTER TABLE `venta` DISABLE KEYS */;
+/*!40000 ALTER TABLE `venta` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-05 23:24:21
--- SQL Base de Datos/query creacion tablas.sql
+++ SQL Base de Datos/query creacion tablas.sql
@@ -1,57 +1,77 @@
-create database inventario;
-use inventario;
+create database sistema_inventario;
+use sistema_inventario;
 
-Create table roles
+Create table rol
 (
 id_rol int auto_increment primary key,
-nombre_rol varchar(50),
+nombre_rol varchar(50) not null,
 descripcion_rol varchar(100)
 );
 
-Create table empleados
+Create table empleado
 (
 id_empleado int auto_increment primary key,
-nombre_empleado varchar(60),
-correo_empleado varchar(60),
+nombre_empleado varchar(60) not null,
+correo_empleado varchar(60) not null unique,
 password_empleado varchar(25),
-id_rol int,
-foreign key (id_rol) references roles(id_rol)
+id_rol int not null,
+foreign key (id_rol) references rol(id_rol)
 );
 
-Create table productos
+Create table producto
 (
 id_producto int auto_increment primary key,
-codigo_serie varchar(50) unique,
+codigo_serie varchar(50) not null unique,
 nombre varchar(50) not null,
 descripcion varchar(80),
-precio decimal(10,2) not null,
-stock int not null,
-ubicacion_almacen varchar(80)
+precio decimal(10,2) not null
 );
 
-Create table ventas
+Create table almacen
+(
+id_almacen int auto_increment primary key,
+nombre_almacen varchar(50) not null,
+ubicacion_almacen varchar(80) not null
+);
+
+Create table inventario
+(
+id_inventario int auto_increment primary key,
+id_producto int not null,
+id_almacen int not null,
+stock int not null,
+ubicacion_interna varchar(80),
+foreign key (id_producto) references producto(id_producto),
+foreign key (id_almacen)  references almacen(id_almacen)
+);
+
+Create table venta
 (
 id_venta int auto_increment primary key,
 fecha datetime not null,
-total decimal(10,2),
-id_empleado int,
-foreign key (id_empleado) references empleados(id_empleado)
+total decimal(10,2) not null,
+id_empleado int not null,
+foreign key (id_empleado) references empleado(id_empleado)
 );
 
 Create table detalle_venta
 (
 id_detalle int auto_increment primary key,
-id_venta int,
-id_producto int,
-foreign key (id_venta) references ventas(id_venta),
-foreign key (id_producto) references productos(id_producto)
+id_venta int not null,
+id_producto int not null,
+foreign key (id_venta) references venta(id_venta),
+foreign key (id_producto) references producto(id_producto)
 );
 
-describe roles;
-describe ventas;
-describe empleados;
+describe rol;
+describe venta;
+describe empleado;
 describe detalle_venta;
-describe productos;
+describe producto;
+describe inventario;
+describe almacen;
+
+
 
 
 
